7d9a8bf573c088b12c8ad33401f6adf3627731b4
Noah <nbelman1@gmail.com>
2025-08-07 12:28:05 -0400

    Complete tasks 1-3 with Team Khai

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-code
+// Code was developed during group coding session
+
+function greeting(username, userRole) {
+    return `Name: ${username}, Role: ${userRole}`;
+}
+
+console.log(greeting('Alexis', 'Student'));
+
+function calculateCosts(numberOfAttendees, costPerAttendee) {
+    let total = numberOfAttendees * costPerAttendee;
+    if (numberOfAttendees > 100) {
+        total  *= 0.9;
+    }
+
+    return total;
+}
+
+console.log('Task Two: ')
+console.log(calculateCosts(120, 15));
+
+function emailValidator(email) 
+{
+    return email.includes("@") 
+    && email.includes(".") 
+    && !email.includes(" ") 
+    && email.lastIndexOf(".") > email.indexOf("@")
+}
+
+console.log('Task Three: ')
+console.log(emailValidator("mike.denney@cooldude.gg"))
